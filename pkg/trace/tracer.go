b9299ece3a2022687d679afb02399eccaf079623
Jack Zheng <jack.zheng@anz.com>
2020-02-13 19:58:40 +1100

    Add tracer package with test

---
+++ pkg/trace/tracer.go
@@ -0,0 +1,27 @@
+package trace
+
+import (
+	"fmt"
+	"io"
+)
+
+// tracer is the interface that describes an object
+// capable of tracing events through the code
+// The Tracer interface defines a function Trace that
+// will accept zero or more arguments of ANY type
+type Tracer interface {
+	Trace(...interface{})
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
